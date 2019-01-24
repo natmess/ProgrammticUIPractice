4a3f0161874d421c0fba16c0e3d6d0d89f44caf2
natmess <nnmessier@gmail.com>
2019-01-24 17:30:20 -0500

    first commit

---
+++ ProgrammaticUIPractice/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  ProgrammaticUIPractice
+//
+//  Created by Nathalie  on 1/24/19.
+//  Copyright © 2019 Nathalie. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ ProgrammaticUIPractice/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  ProgrammaticUIPractice
+//
+//  Created by Nathalie  on 1/24/19.
+//  Copyright © 2019 Nathalie. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
